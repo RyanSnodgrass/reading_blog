ebcbb0529785564f409c40ae5c81879ee302e913
Ryan Snodgrass <ryansnodgrass.dev@gmail.com>
2024-12-12 16:06:26 -0500

    add extra fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'sans': ['lato', 'ui-sans-serif', 'system-ui'],
+        'sans': ['Karla', 'ui-sans-serif', 'system-ui'],
         'serif': ['playfair display'],
         'navbar': ['lato']
       },
