cfddf0fe151478dc6af0b87e37aa6aa932dc6cee
Ryan Snodgrass <ryansnodgrass.dev@gmail.com>
2025-07-15 16:01:02 -0400

    webpack working when you run it in a separate terminal

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './source/javascripts/index.js',
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: 'site.js',
+    path: path.resolve(__dirname, 'source/javascripts'),
   },
 };
